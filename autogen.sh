e4c1eebd11487d5b912a2fc0493c16dd02ebd66b
Bob Remeika <bremeika@yammer-inc.com>
2012-10-02 21:11:59 -0700

    Test

--- autogen.sh
+++ autogen.sh
@@ -3,7 +3,7 @@
 echo "Generating configure files... may take a while."
 
 autoreconf --install --force && \
-  echo "Preparing was successful if there was no error messages above." && \
+  echo "Preparing was successful if there were no error messages above." && \
   echo "Now type:" && \
   echo "  ./configure && make"  && \
   echo "Run './configure --help' for more information"
