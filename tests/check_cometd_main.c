416e0314701bcb23e1669b664243cd9c57a0de90
Bob Remeika <bremeika@yammer-inc.com>
2013-12-04 23:31:42 -0800

    This backtrace thing is pretty much useless

--- tests/check_cometd_main.c
+++ tests/check_cometd_main.c
@@ -1,33 +1,10 @@
 #include <check.h>
-#include <signal.h>
-#include <execinfo.h>
-#include <stdio.h>
-#include <unistd.h>
 #include <stdlib.h>
-
 #include "check_cometd.h"
-
-void error_handler(int sig);
-
-void
-error_handler(int sig)
-{
-  void *array[10];
-  size_t size;
-
-  // get void*'s for all entries on the stack
-  size = backtrace(array, 10);
-
-  // print out all the frames to stderr
-  fprintf(stderr, "Error: signal %d:\n", sig);
-  backtrace_symbols_fd(array, size, STDERR_FILENO);
-  exit(1);
-}
 
 int
 main (void)
 {
-  signal(SIGSEGV, error_handler);
   g_type_init();
 
   int number_failed;
